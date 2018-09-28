0a4fb065ec2551562537da75d5f93681f4a31f16
jiangshaojie <jiangsj3@gmail.com>
2018-09-28 09:00:10 +0800

    prefork

---
+++ chapter13/client.py
@@ -0,0 +1,41 @@
+#coding:utf-8
+import json
+import time
+import struct
+import socket
+from multiprocessing import Pool
+
+def rpc(sock,in_,params):
+    response=json.dumps({"in":in_,"params":params})
+    length_prefix=struct.pack("I",len(response))
+    sock.sendall(length_prefix)
+    sock.sendall(bytes(response,encoding='utf-8'))
+    length_prefix=sock.recv(4)
+    length,=struct.unpack("I",length_prefix)
+    body=sock.recv(length)
+    response=json.loads(body)
+    return response["out"],response["result"]
+def clientrequest(s):
+
+    for i in range(100):
+        out,result=rpc(s,"ping","ireader %d" % i)
+        print(out,result)
+        # time.sleep(1)
+
+def sss(s):
+    pool = Pool(4)
+    # for i in range(10):
+    for i in range(10):
+        p = pool.apply_async(func=clientrequest, args=(s,))
+    pool.close()
+    pool.join()
+if __name__=="__main__":
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect(('127.0.0.1',8080))
+    sss(s)
+    # pool.start
+    # for i in range(10):
+    #     out,result=rpc(s,"ping","ireader %d" % i)
+    #     print(out,result)
+    #     time.sleep(2)
+    s.close()
